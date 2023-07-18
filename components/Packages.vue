03fdb9cf7d0909d4f693135c235e2473f19269a8
joshii0630 <127636476+joshii0630@users.noreply.github.com>
2023-07-18 17:47:22 +0800

    done except carousel

--- components/Packages.vue
+++ components/Packages.vue
@@ -2,23 +2,23 @@
   <div class="BG w-full bg-[url('../assets/bg/BG2.png')] bg-no-repeat bg-cover cursor-default" id="packages">
     <div class="w-full justify-center">
       <div class="w-full flex mx-auto justify-center">
-        <h1 class="my-10 text-7xl text-[#4AA3AA] px-3 font-black uppercase text-outline">Packages</h1>
+        <h1 class="my-10 lg:text-7xl text-5xl text-[#4AA3AA] px-3 font-black uppercase text-outline">Packages</h1>
       </div>
-      <div class="w-full flex mx-auto justify-center items-stretch">
+      <div class="w-full flex flex-wrap mx-auto justify-center items-stretch">
 
-        <div class="w-1/4 h-80 p-7 bg-[#A7D5D8] m-4 rounded-3xl self-center text-center dropshadow animate-zoom cursor-pointer">
+        <div class="lg:w-1/4 lg:h-80 p-7 bg-[#A7D5D8] m-4 rounded-3xl self-center text-center dropshadow animate-zoom cursor-pointer">
           <p class="text-3xl mb-7 font-bold">Online Sessions</p>
           <p class="text-lg">Lorem ipsum dolor sit amet. Id consequuntur adipisci et voluptatem quia
             aut unde voluptas. Qui tempora enim ex dolor animi ab deleniti laudantium.</p>
         </div>
 
-        <div class="w-1/4 h-96 p-7 bg-[#6DB3B8] m-4 rounded-3xl self-center text-center dropshadow animate-zoom cursor-pointer">
+        <div class="lg:w-1/4 lg:h-96 p-7 bg-[#6DB3B8] m-4 rounded-3xl self-center text-center dropshadow animate-zoom cursor-pointer">
           <p class="text-3xl mb-7 font-bold">On-Site Sessions</p>
           <p class="text-lg">Lorem ipsum dolor sit amet. Id consequuntur adipisci et voluptatem quia 
             aut unde voluptas. Qui tempora enim ex dolor animi ab deleniti laudantium.</p>
         </div>
 
-        <div class="w-1/4 h-80 p-7 bg-[#A7D5D8] m-4 rounded-3xl self-center text-center dropshadow animate-zoom cursor-pointer">
+        <div class="lg:w-1/4 lg:h-80 p-7 bg-[#A7D5D8] m-4 rounded-3xl self-center text-center dropshadow animate-zoom cursor-pointer">
           <p class="text-3xl mb-7 font-bold">Advanced Package</p>
           <p class="text-lg">Lorem ipsum dolor sit amet. Id consequuntur adipisci et voluptatem quia 
             aut unde voluptas. Qui tempora enim ex dolor animi ab deleniti laudantium.</p>

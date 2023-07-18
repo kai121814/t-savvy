891491ef53666c0d0315105b7d28f63beba561f8
Michael Earvin Tia <mictt123@gmail.com>
2023-07-18 17:22:53 +0800

    testimonies update

--- components/Testimonies.vue
+++ components/Testimonies.vue
@@ -1,38 +1,36 @@
 <template>
   <div class="BG flex flex-col items-center mx-auto cursor-default" id="testimonies">
-    <h1 class=" text-6xl font-bold text-[#4AA3AA] "><span class="text-outline"></span><span
-          class="text-[#F5F0BB] px-3 bg-gradient-to-r to-[#9FBE6C] from-[#4AA3AA]">Testimonies</span></h1>
+    <h1 class="text-4xl md:text-6xl font-bold text-[#4AA3AA]">
+      <span class="text-outline"></span>
+      <span class="text-[#F5F0BB] px-3 bg-gradient-to-r to-[#9FBE6C] from-[#4AA3AA]">Testimonies</span>
+    </h1>
     <div class="border align-end relative custom-border">
-      <div class="w-52 h-52 rounded-full bg-[#B3C890] -ml-8 absolute top-1/2 left-0 transform -translate-y-1/2"></div>
-      This Code Camp has helped me get the job of my dreams.
+      <div class="circle"></div>
+      <p class="testimonial-text text-sm md:text-3xl md:pl-1 pl-16">This Code Camp has helped me get the job of my dreams.</p>
     </div>
     <div class="border align-left relative custom-border">
       <div class="circle2"></div>
-      This Code Camp has helped me get the job of my dreams.
+      <p class="testimonial-text md:pr-1 pr-16">This Code Camp has helped me get the job of my dreams.</p>
     </div>
     <div class="border align-end relative custom-border">
-      <div class="w-52 h-52 rounded-full bg-[#B3C890] -ml-8 absolute top-1/2 left-0 transform -translate-y-1/2 "></div>
-      This Code Camp has helped me get the job of my dreams.
+      <div class="circle"></div>
+      <p class="testimonial-text text-sm md:text-3xl md:pl-1 pl-16">This Code Camp has helped me get the job of my dreams.</p>
     </div>
   </div>
 </template>
 
 <style>
-
 .circle {
-  @apply w-52 h-52 rounded-full bg-[#B3C890] -ml-8 absolute top-1/2 left-0 transform -translate-y-1/2 ;
+  @apply w-24 h-24 md:w-52 md:h-52 rounded-full bg-[#B3C890] -ml-8 absolute top-1/2 left-0 transform -translate-y-1/2;
 }
 
 .circle2 {
-  @apply w-52 h-52 rounded-full bg-[#B3C890] -mr-8 absolute top-1/2 right-0 transform -translate-y-1/2;
+  @apply w-24 h-24 md:w-52 md:h-52 rounded-full bg-[#B3C890] -mr-8 absolute top-1/2 right-0 transform -translate-y-1/2;
 }
 
 .custom-border {
-  @apply border border-4  border-[#B3C890] bg-[#DBDFAA] py-12 px-5 rounded-md relative mb-8 w-11/12 font-sans my-10 text-3xl;
-  border-top-left-radius: 40px;
-  border-bottom-left-radius: 40px;
-  border-top-right-radius: 40px;
-  border-bottom-right-radius: 40px;
+  @apply border border-4 border-[#B3C890] bg-[#DBDFAA] py-4 md:py-12 px-3 md:px-5 rounded-md relative mb-4 md:mb-8 w-11/12 font-sans text-sm md:text-3xl;
+  border-radius: 40px;
 }
 
 .align-end {
@@ -43,11 +41,15 @@
   @apply text-left;
 }
 
+.testimonial-text {
+  @apply mt-2 md:mt-4;
+}
+
 .title {
   @apply uppercase text-teal-500 outline-white outline-2 text-5xl font-bold;
 }
-.BG{
-  padding-bottom: 5%;
-  padding-top: 5%;
+
+.BG {
+  @apply py-5;
 }
 </style>
